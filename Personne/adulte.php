5cd418e5f2cd037e42699549318fdd4bd8822547
Brice NGABIRANO <ngablanco234@gmail.com>
2020-07-21 23:33:12 +0200

    attribut et constructeur de la classe Adulte

--- Personne/adulte.php
+++ Personne/adulte.php
@@ -6,4 +6,16 @@
         //Attribut
         private $type;
         private $marie;
+
+        //Constructeur
+        public function __construct($personne,$type,$marie)
+        {
+            $this->nom = $personne->nom;
+            $this->prenom = $personne->prenom;
+            $this->age = $personne->age;
+            $this->sexe = $personne->sexe;
+            $this->fonction = $personne->fonction;
+            $this->type = $type;
+            $this->marie = $marie;
+        }
     }

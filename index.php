3c51c04327814c611fe42a16bf95bb9f2b8d246b
Brice NGABIRANO <ngablanco234@gmail.com>
2020-07-21 23:44:04 +0200

    creation des objets de la classe Personne

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
         require 'Personne/personne.php';
         require 'Personne/Adulte.php';
         require 'Personne/Enfant.php';
+
+        $personne1 = new Personne("NGABIRANO","Brice",25,"M","Etudiant");
+        $personne2 = new Personne("NGABO","Vladimir",20,"M","Etudiant");
+        $personne3 = new Personne("NAHAYO","Beatrice",25,"F","Secretaire");
+        $personne4 = new Personne("NTAWE","Patrice",60,"M","Sans");
     ?>
 </body>
 </html>

611269ee83dc11b7a76fda3b8f08bead70131cb2
Brice NGABIRANO <ngablanco234@gmail.com>
2020-07-22 01:01:43 +0200

    test sur les objets des classes filles Enfant et Adulte

--- index.php
+++ index.php
@@ -8,14 +8,21 @@
 <body>
     <?php
         require 'Personne/personne.php';
+        require 'Personne/enfant.php';
+        require 'Personne/adulte.php';
 
         $personne1 = new Personne("NGABIRANO","Brice",25,"M","Etudiant");
-        $personne2 = new Personne("NGABO","Vladimir",20,"M","Etudiant");
+        $personne2 = new Personne("NGABO","Vladimir",10,"M","Ecolier");
         $personne3 = new Personne("NAHAYO","Beatrice",25,"F","Secretaire");
         $personne4 = new Personne("NTAWE","Patrice",60,"M","Sans");
+        $enfant = new Enfant($personne2,"Enfant");
+        $adulte = new Adulte($personne1,"Adulte","Celibataire");
 
         //Test
-        echo $personne1->getIdentite();
+        echo $personne1->getIdentite().'<hr>';
+        echo $enfant->getIdentite().'<br>'.$enfant->typePersonne().'<hr>';
+        echo $adulte->getIdentite().'<br>'.$adulte->typePersonne().'<br> Etat-Civil: '.$adulte->getEtatcivil();
+
     ?>
 </body>
 </html>

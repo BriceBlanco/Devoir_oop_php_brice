a8f70988454b49b3129ac45f424b9d76b05972a0
Brice NGABIRANO <ngablanco234@gmail.com>
2020-07-21 23:27:54 +0200

    constructeur de la classe Enfant

--- Personne/enfant.php
+++ Personne/enfant.php
@@ -5,4 +5,15 @@
     {
         //Attribut
         private $type;
+
+        //Constructeur
+        public function __construct($personne,$type)
+        {
+            $this->nom = $personne->nom;
+            $this->prenom = $personne->prenom;
+            $this->age = $personne->age;
+            $this->sexe = $personne->sexe;
+            $this->fonction = $personne->fonction;
+            $this->type = $type;
+        }
     }
